d8957e5b4e4c64b8022d520a74e43e9dce243d32
RusAnonym <alexandrsemin2033@gmail.com>
2023-05-25 14:13:42 +0300

    update security incident schema

--- src/lib/DB/schemes/securityIncident.ts
+++ src/lib/DB/schemes/securityIncident.ts
@@ -7,12 +7,35 @@ const SECURITY_INCIDENTS_TYPES = [
     "user-not-found",
     "area-not-found",
     "area-is-locked",
-    "unauthorized-user"
+    "unauthorized-user",
+    "new-entrance-without-exit"
 ] as const;
+
+const securityIncidentCreatorAcs = Type.Object({
+    type: Type.Literal("acs")
+});
+
+const securityIncidentCreatorUser = Type.Object({
+    type: Type.Literal("user"),
+    userId: Type.Number()
+});
+
+const securityIncidentCreator = Type.Union([
+    securityIncidentCreatorAcs,
+    securityIncidentCreatorUser
+]);
+
+type TSecurityIncidentCreatorBox = Static<typeof securityIncidentCreator>;
 
 const securityIncidentBox = Type.Object({
     id: Type.Number(),
-    type: Type.Union(SECURITY_INCIDENTS_TYPES.map(x => Type.Literal(x)))
+    type: Type.Union(SECURITY_INCIDENTS_TYPES.map(x => Type.Literal(x))),
+    userId: Type.Number(),
+    deviceId: Type.Optional(Type.Number()),
+    passLogId: Type.Optional(Type.Number()),
+    message: Type.Optional(Type.String()),
+    creator: securityIncidentCreator,
+    createdAt: Type.Date()
 });
 
 type TSecurityIncidentBox = Static<typeof securityIncidentBox>;
@@ -25,6 +48,42 @@ const securityIncidentSchema = new Schema<TSecurityIncidentBox>({
     },
     type: {
         type: Schema.Types.String,
+        required: true
+    },
+    userId: {
+        type: Schema.Types.Number,
+        required: true
+    },
+    deviceId: {
+        type: Schema.Types.Number,
+        required: false
+    },
+    passLogId: {
+        type: Schema.Types.Number,
+        required: false
+    },
+    message: {
+        type: Schema.Types.String,
+        required: false
+    },
+    creator: {
+        type: new Schema<TSecurityIncidentCreatorBox>({
+            type: {
+                type: Schema.Types.String,
+                required: true
+            },
+            userId: {
+                type: Schema.Types.Number,
+                required: false
+            }
+        }, {
+            versionKey: false,
+            _id: false
+        }),
+        required: true
+    },
+    createdAt: {
+        type: Schema.Types.Date,
         required: true
     }
 }, {
