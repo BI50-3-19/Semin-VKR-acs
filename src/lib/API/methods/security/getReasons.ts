3ed5b2b9655c11be6c297baf4e76be98b25fe27f
RusAnonym <alexandrsemin2033@gmail.com>
2023-05-27 15:05:00 +0300

    Refactor security.getReasons API route schema to remove unused properties
    This commit removes the count and offset properties from the schema of the security.getReasons API route and refactors the implementation to use lean aggregation for efficiency.

--- src/lib/API/methods/security/getReasons.ts
+++ src/lib/API/methods/security/getReasons.ts
@@ -2,32 +2,18 @@ import server from "../..";
 import DB from "../../../DB";
 import APIError from "../../Error";
 
-import { Type } from "@sinclair/typebox";
-
-server.post("/security.getReasons", {
-    schema: {
-        body: Type.Object({
-            count: Type.Optional(Type.Number({ minimum: 1, maximum: 200 })),
-            offset: Type.Optional(Type.Number({ minimum: 0 }))
-        })
-    }
-}, async (request) => {
+server.post("/security.getReasons", async (request) => {
     if (!request.userHasAccess("security")) {
         throw new APIError({
             code: 8, request
         });
     }
 
-    const { count, offset } = request.body;
-
     const reasons = await DB.securityReasons.find({}, {
         _id: false,
         id: true,
         title: true
-    }, {
-        limit: count,
-        skip: offset
-    });
+    }).lean();
 
     return reasons;
 });
